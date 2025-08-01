6696cb504fbd1edf3930effae4af3696becbda39
Danyl <kotvasilij1@gmail.com>
2025-08-01 17:32:33 +0300

    feature: ServiceSettings + modal + dropDown

--- src/features/Settings/ServiceSettings/ServiceSettings.tsx
+++ src/features/Settings/ServiceSettings/ServiceSettings.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { mockServices } from "@/mock/Service/servicesMock";
 import { ServiceList } from "@/components/Settings/ServiceSettings/ServiceList";
 import { Service, ServiceType } from "@/types/service";
+import { ServiceFormModal } from "@/components/Settings/ServiceSettings/ServiceFormModal";
 
 export function ServiceSettings() {
   const { role } = useParams();
@@ -20,32 +21,6 @@ export function ServiceSettings() {
     setServices((prev) => prev.filter((service) => service.id !== id));
   };
 
-  const handleEdit = (updated: Service) => {
-    setServices((prev) =>
-      prev.map((service) => (service.id === updated.id ? updated : service))
-    );
-  };
-
-  const handleAdd = (type: ServiceType) => {
-    const name = prompt("Назва послуги");
-    const unit = prompt("Одиниця виміру");
-    const price = parseFloat(prompt("Ціна") || "0");
-    const amount = parseFloat(prompt("Кількість") || "1");
-
-    if (!name || !unit) return;
-
-    const newService: Service = {
-      id: crypto.randomUUID(),
-      name,
-      unit,
-      price,
-      amount,
-      serviceType: type,
-    };
-
-    setServices((prev) => [...prev, newService]);
-  };
-
   const mainServices = services.filter(
     (s) => s.serviceType === "Основні послуги"
   );
@@ -53,27 +28,64 @@ export function ServiceSettings() {
     (s) => s.serviceType === "Додаткові роботи"
   );
 
+  // modal window
+  const [modalData, setModalData] = useState<{
+    service?: Service;
+    type: ServiceType;
+  } | null>(null);
+
+  const openEditModal = (service: Service) => {
+    setModalData({ service, type: service.serviceType });
+  };
+
+  const openAddModal = (type: ServiceType) => {
+    setModalData({ type });
+  };
+
+  const handleSave = (newService: Service) => {
+    setServices((prev) => {
+      const exists = prev.find((s) => s.id === newService.id);
+      if (exists) {
+        return prev.map((s) => (s.id === newService.id ? newService : s));
+      }
+
+      return [
+        ...prev,
+        { ...newService, serviceType: modalData?.type || "Основні послуги" },
+      ];
+    });
+    setModalData(null);
+  };
+
   return (
     <section
-      className={`max-w-[1126px] m-auto md:pt-[60px] md:pr-[80px] md:pl-[60px] md:pb-[40px] pt-[40px] pr-[14px] pb-[74px] pl-[26px]`}
+      className={`relative max-w-[1126px] m-auto md:pt-[60px] md:pr-[80px] md:pl-[60px] md:pb-[40px] pt-[40px] pr-[14px] pb-[74px] pl-[26px]`}
     >
       <div className={`flex flex-col md:gap-[60px] gap-[40px]`}>
         <ServiceList
           services={mainServices}
           type="Основні послуги"
           onDelete={handleDelete}
-          onEdit={handleEdit}
-          onAdd={() => handleAdd("Основні послуги")}
+          onEdit={openEditModal}
+          onAdd={() => openAddModal("Основні послуги")}
         />
 
         <ServiceList
           services={additionalServices}
           type="Додаткові роботи"
           onDelete={handleDelete}
-          onEdit={handleEdit}
-          onAdd={() => handleAdd("Додаткові роботи")}
+          onEdit={openEditModal}
+          onAdd={() => openAddModal("Додаткові роботи")}
         />
       </div>
+      {modalData && (
+        <ServiceFormModal
+          service={modalData.service}
+          type={modalData.type}
+          onClose={() => setModalData(null)}
+          onSave={handleSave}
+        />
+      )}
     </section>
   );
 }
