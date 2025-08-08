a26a6b71af38cd33e1eb43e2b7f9f4ebbd7184ae
Danyl <kotvasilij1@gmail.com>
2025-08-08 14:06:23 +0300

    feature: projectCrewWorkers

---
+++ src/components/Project/ProjectsDetailed/ProjectCrewWorkersTable/ProjectCrewWorkersTable.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import { Table } from "@/components/Table/Table";
+import { Worker } from "@/types/worker";
+import { Crew } from "@/types/crew";
+
+interface ProjectCrewWorkersTableProps {
+  workers: Worker[];
+  crewId: string;
+  crews: Crew[];
+  enableTooltips?: boolean;
+}
+
+export function ProjectCrewWorkersTable({
+  workers,
+  crewId,
+  enableTooltips = true,
+}: ProjectCrewWorkersTableProps) {
+  const crewWorkers = workers.filter((worker) => worker.crewId === crewId);
+
+  return (
+    <div className="mb-[40px]">
+      <Table
+        data={crewWorkers}
+        className="ProjectCrewWorkersTableWrap"
+        showIndex={true}
+        enableTooltips={enableTooltips}
+        columns={[
+          {
+            key: "name",
+            label: "ПІБ виконавця",
+            tooltip: (worker) => `ПІБ виконавця: ${worker.name}`,
+          },
+          {
+            key: "occupation",
+            label: "Посада",
+          },
+          {
+            key: "salary",
+            label: "Зарплата",
+          },
+          {
+            key: "phone",
+            label: "Контакти",
+            tooltip: (worker) => `Контакти: ${worker.phone}`,
+          },
+        ]}
+      />
+    </div>
+  );
+}
--- src/features/projects/ProjectsDetailed/ProjectsDetailed.tsx
+++ src/features/projects/ProjectsDetailed/ProjectsDetailed.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
 import { mockProjects } from "@/mock/Project/mockProjects";
+import { mockWorkers } from "@/mock/Workers/workersMock";
+import { mockCrews } from "@/mock/Crew/crewMock";
+import { ProjectCrew } from "@/components/Project/ProjectsDetailed/ProjectCrew";
 
 interface Props {
   projectId: string;
@@ -8,10 +11,24 @@ interface Props {
 
 export function ProjectsDetailed({ projectId }: Props) {
   const project = mockProjects.find((c) => c.id === projectId);
-
   if (!project) {
     return <div>ProjectsDetailed не знайдено</div>;
   }
 
-  return <div className="pl-[10px] pr-[10px] pt-[76px]"></div>;
+  return (
+    <div className="pl-[10px] pr-[10px] pt-[76px] pb-[40px] md:pl-[80px] md:pr-[56px] md:pt-[60px] md:pb-[48px] ">
+      <div>
+        <div>ProjectsInfo</div>
+        <div>ProjectEstimate</div>
+        <div>ProjectMaterials</div>
+        <div>ProjectPayment</div>
+        <ProjectCrew
+          project={project}
+          crews={mockCrews}
+          workers={mockWorkers}
+        />
+        <div>ProjectNotes</div>
+      </div>
+    </div>
+  );
 }
