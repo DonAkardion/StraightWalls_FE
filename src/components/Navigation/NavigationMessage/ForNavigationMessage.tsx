bcf795884699f7e59d6c080744c3d43f0e47cb44
VladHolovin27 <vladislavg899@gmail.com>
2025-09-08 15:17:20 +0300

    notes page/message

--- src/components/Navigation/NavigationMessage/ForNavigationMessage.tsx
+++ src/components/Navigation/NavigationMessage/ForNavigationMessage.tsx
@@ -1,21 +1,69 @@
-import React from "react";
-import { mockMessages } from "@/mock/NavigationMessage/navigationMessage";
+import React, { useEffect, useState } from "react";
+import { useUser } from "@/context/UserContextProvider";
+import { getProjectPaymentsOverdue, Payment } from "@/api/payments";
+import { ProjectDetailedResponse } from "@/types/project";
+import { getProjectById } from "@/api/projects";
 
-export const ForNavigationMessage = () => {
+interface NavigationMessage {
+  id: number;
+}
+
+interface Interaction {
+  projectsMessage: NavigationMessage[];
+}
+
+interface PaymentWithProject {
+  payment: Payment;
+  project: ProjectDetailedResponse;
+}
+
+export const ForNavigationMessage = ({ projectsMessage }: Interaction) => {
+  const { token } = useUser();
+  const [combinedData, setCombinedData] = useState<PaymentWithProject[]>([]);
+
+  useEffect(() => {
+    if (!token) return;
+
+    const fetchData = async () => {
+      try {
+        const results: PaymentWithProject[] = [];
+
+        for (const project of projectsMessage) {
+          const paymentResponse: any = await getProjectPaymentsOverdue(project.id, token);
+          const paymentsArray: Payment[] = paymentResponse.data ? paymentResponse.data : [paymentResponse];
+          const projectResponse = await getProjectById(project.id, token);
+
+          paymentsArray.forEach((payment) => {
+            results.push({ payment, project: projectResponse });
+          });
+        }
+
+        setCombinedData(results);
+      } catch (error) {
+        console.log("Error fetching navigation messages:", error);
+      }
+    };
+
+    fetchData();
+  }, [projectsMessage, token]);
+
   return (
     <div className="px-2 mt-4 text-[14px] font-inter text-black space-y-2 text-center">
-      {mockMessages.map((interaction, index) => (
+      {combinedData.map((item, index) => (
         <div key={index}>
           {index === 0 && <div className="border-t border-[#FFB326] mb-1" />}
 
           <div className="pb-2">
             <div>
-              <span>{interaction.title}</span>
+              <span className="font-semibold">{item.project.name}</span>
             </div>
             <div>
               <span className="text-red-600 font-semibold">
-                {interaction.price} грн
+                {item.payment.amount} грн
               </span>
+            </div>
+            <div>
+              <span className="font-semibold">{item.project.description}</span>
             </div>
           </div>
 
--- src/components/Notifications/components/Notification.tsx
+++ src/components/Notifications/components/Notification.tsx
@@ -1,24 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Notification.module.css";
+import { useUser } from "@/context/UserContextProvider";
+import {  getProjectPaymentsOverdue, Payment } from "@/api/payments";
+import { getProjectById } from "@/api/projects";
+import { ProjectDetailedResponse } from "@/types/project";
 
-export const Notification = () => {
+interface NotificationComponentProps {
+  id: number;
+}
+
+interface Notification {
+  notes: NotificationComponentProps[];
+}
+
+interface PaymentWithProject {
+  payment: Payment;
+  project: ProjectDetailedResponse;
+}
+
+export const Notification = ({ notes }: Notification) => {
+  const { token } = useUser();
+  const [note, setNote] = useState<PaymentWithProject[]>([]);
+
+  const formatDate = (date: string | Date) => {
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
+  useEffect(() => {
+      if (!token) return;
+  
+      const fetchData = async () => {
+        try {
+          const results: PaymentWithProject[] = [];
+  
+          for (const note of notes) {
+            const paymentResponse: any = await getProjectPaymentsOverdue(note.id, token);
+            const paymentsArray: Payment[] = paymentResponse.data ? paymentResponse.data : [paymentResponse];
+            const projectResponse = await getProjectById(note.id, token);
+  
+            paymentsArray.forEach((payment) => {
+              results.push({ payment, project: projectResponse });
+            });
+          }
+  
+          setNote(results);
+        } catch (error) {
+          console.log("Error fetching navigation messages:", error);
+        }
+      };
+  
+      fetchData();
+    }, [notes, token]);
+
   return (
     <div
-      className={`${styles.notification} pt-[20px] pr-[30px] pb-[20px] pl-[14px] md:pt-[20px] md:pr-[40px] md:pb-[26px] md:pl-[30px] rounded-[5px] flex flex-col gap-[15px] `}
+      className={`${styles.notification} pt-[20px] pr-[30px] pb-[20px] pl-[14px] md:pt-[20px] md:pr-[40px] md:pb-[26px] md:pl-[30px] rounded-[5px] flex flex-col gap-[15px]`}
     >
-      <div
-        className={`${styles.notificationText} flex justify-between gap-[16px]`}
-      >
-        <div className={`${styles.notificationsTextTytle} `}>
-          <h3>Водій взяв замовлення №123</h3>
-        </div>
-        <div className={`${styles.notificationDate} `}>
-          <h6>14.07.2025</h6>
-        </div>
-      </div>
-      <div className={`${styles.notificationsTextContent} `}>
-        <h6>Водій Петро Іванович взяв замволення №123</h6>
-      </div>
+      {note.map((payment, index) => {
+        const noteDate = payment.payment.due_date ?? formatDate(payment.payment.updated_at);
+
+        return (
+          <div key={index}>
+            <div
+              className={`${styles.notificationText} flex justify-between gap-[16px]`}
+            >
+              <div className={`${styles.notificationsTextTytle}`}>
+                <h3>{payment.project.name}</h3>
+              </div>
+              <div className={`${styles.notificationDate}`}>
+                <h6>{noteDate}</h6>
+              </div>
+            </div>
+            <div className='flex justify-between items-center'>
+              <div className={`${styles.notificationsTextContent}`}>
+              <h6>{payment.payment.description}</h6>
+            </div>
+            <div className={`${styles.notificationDate}`}>
+                <h6>{payment.payment.amount}</h6>
+            </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
